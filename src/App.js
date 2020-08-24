7fbb37462c52866b66df3165dd5ffd7013807be5
Ben Zelinski <bziggz@gmail.com>
2020-08-24 18:39:02 -0400

    Table of Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import DATA from './data.js';
+// import Table from './components/Table';
+
 class App extends Component {
   render() {
     return (
@@ -12,10 +15,45 @@ class App extends Component {
           <p>
             Welcome to the app!
           </p>
-        </section>
+          <Table />
+        </section>     
       </div>
     );
   }
 }
 
+class Table extends Component {
+  routeRows = () => {
+    return DATA.routes.map((route, idx) => {
+      return (
+        <tr key={idx}>
+          <td>{route.airline}</td>
+          <td>{route.src}</td>
+          <td>{route.dest}</td>
+        </tr>
+      )
+    });
+  };
+
+  state = {
+    routes: this.routeRows(),
+  };
+  
+  render() {
+    return (
+      <div>
+        <table className='table'>
+          <thead>
+            <td>Airline</td>
+            <td>Source</td>
+            <td>Destination</td>
+          </thead>
+          <tbody>
+            {this.state.routes}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 export default App;
