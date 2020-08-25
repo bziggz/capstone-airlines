a8330d64038028ecbc0cb0fe75a55c30dd3e036c
Ben Zelinski <bziggz@gmail.com>
2020-08-24 20:24:51 -0400

    Adding Route Pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,18 +5,38 @@ import DATA from '../data.js';
 class Table extends Component {
   static defaultProps = {
     format(property, value) { value },
+    perPage: 25,
   }
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      page: 0,
+    }
   }
+
+  nextPage = (e) => {
+    e.preventDefault();
+    this.setState({ page: this.state.page + 1 });
+  };
+
+  previousPage = (e) => {
+    e.preventDefault();
+    this.setState({ page: this.state.page - 1 });
+  };
 
   render () {
     const tableHeaders = this.props.columns.map((column) => {
       return <th key={column.name}>{column.name}</th>
     });
 
-    const tableRows = this.props.rows.map((row) => {
+    const pageStart = this.state.page * this.props.perPage;
+
+    const pageEnd = pageStart + 25 > this.props.rows.length ? 
+                      this.props.rows.length : pageStart + 25;
+
+    const tableRows = this.props.rows.slice(pageStart, pageEnd).map((row) => {
       const rowData = this.props.columns.map((column) => {
         const value = row[column.property];
         return <td key={column.property + value}>
@@ -37,6 +57,29 @@ class Table extends Component {
             {tableRows}
           </tbody>
         </table>
+        <div id='pagination'>
+          <p>
+            Showing {pageStart + 1} - {pageEnd} of {this.props.rows.length} routes.
+          </p>
+          <p>
+            <button
+              key='previous'
+              disabled={this.state.page === 0} 
+              onClick={this.previousPage}
+            >
+              Previous
+            </button>
+          </p>
+          <p>
+            <button
+              key='next'
+              disabled={pageEnd >= this.props.rows.length} 
+              onClick={this.nextPage}
+            >
+              Next
+            </button>
+          </p>
+        </div>
       </div>
     );
   }
